093aa93a4129be88a19783583f4c439f165595b4
Levan Jandieri <le.jandieri@gmail.com>
2019-05-08 14:07:44 +0200

    Update TSP-GA2.py

--- TSP-GA2.py
+++ TSP-GA2.py
@@ -1,15 +1,15 @@
 import math, random, sys, numpy, matplotlib.pyplot as plt
 
-class City:
+class City: 
     nc = 0 ## number of city instances
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.id = City.nc
+        self.id = City.nc ## self-generated with static variable nc
         self.name = "City" + str(City.nc)
         City.nc += 1
     
-    def distance(self,nextCity):
+    def distance(self,nextCity): ## calculates distance between two cities 
         x_difference = self.x - nextCity.x
         y_difference = self.y - nextCity.y 
         result = math.sqrt(x_difference**2 + y_difference**2)
@@ -19,12 +19,12 @@ class City:
     def __repr__(self):
         return repr([self.name,self.x,self.y])
 
-def generateRoute(cities):
+def generateRoute(cities): ## generate a random route using cities
     route = random.sample(cities,len(cities))
 
     return route
 
-def readCityFile(f):
+def readCityFile(f): ## read file with cities
     fi = open(default_parameters[6],"r")
     cities = []
 
@@ -38,17 +38,19 @@ def readCityFile(f):
     return cities
 
 
-class Individual:
+class Individual: ## individual with a route
     count = 0
+
     def __init__(self,route): 
         self.route = route
         self.totalDistance = 0
         self.totalDistance = self.getDistance()
-        self.fitness = float(1 / self.totalDistance)
-        self.id = Individual.count
+        ## fitness is equal to one over the total distance of the individual's route
+        self.fitness = float(1 / self.totalDistance) 
+        self.id = Individual.count ## self-generated with variable count
         Individual.count += 1
     
-    def getDistance(self):
+    def getDistance(self): ## return total distance of route
         routeLength = len(self.route)
         dist = 0
         for i in range(0,routeLength):
@@ -62,22 +64,25 @@ class Individual:
 
         return dist
     
-    def breed(self,parent2):
+    def breed(self,parent2): ## "breeds" self with parent2 
         gene1 = [] # inherited from self
         gene2 = [] # inherited from parent2
         l = len(self.route)
 
         n1 = int(random.random() * l) 
         n2 = int(random.random() * l)
-
-        start = min(n1,n2)
+        start = min(n1,n2) 
         end = max(n1,n2)
 
-        for i in range(start,end):
+        for i in range(start,end): ## the subroute [start,end] is retained from first parent
             gene1.append(self.route[i])
-        gene2 = [c for c in parent2.route if c not in gene1]
-        res = gene1 + gene2
+
+        ## take cities from parent2 that have not been retained from parent1
+        gene2 = [c for c in parent2.route if c not in gene1] 
+        res = gene1 + gene2 ## questionable
         child = Individual(res)
+
+        ### FIX THIS ###
         ##for i in range(len(gene1)):
         ##    gene2.insert(start+i,gene1[i])
         ##child = Individual(gene2)
@@ -87,20 +92,20 @@ class Individual:
     def __repr__(self):
         return repr(self.route)
 
-class generation():
+class generation(): ## generation consists of a list of individuals
     gen_id = 0
 
-    def __init__(self,newGen):
+    def __init__(self,newGen): ## newGen is list of individuals
         self.gen_id = generation.gen_id
         generation.gen_id = generation.gen_id + 1
         self.population = []
-        self.population.extend(newGen)
+        self.population.extend(newGen) 
         self.getMean()
         self.getStandardDeviation()
         self.getMin()
-        self.selectPool()
+        self.selectPool() 
 
-    def getMean(self):
+    def getMean(self): 
         sum = 0
         popLen = len(self.population)
 
@@ -110,7 +115,7 @@ class generation():
 
         return self.mean
 
-    def getStandardDeviation(self):
+    def getStandardDeviation(self): 
         popLen = len(self.population)
         mean = self.getMean() 
         sum = 0
@@ -127,35 +132,38 @@ class generation():
         return self.minimum
 
     ### use sort instead of standard Deviation
-    def selectPool(self): 
+    def selectPool(self): ## select individuals that will procreate
 
         self.selected = []
         self.elite = []
         sortedPool = self.population
-        sortedPool.sort(key=lambda x: x.fitness, reverse=True)
-        sortedFitness = [c.fitness for c in sortedPool]
-        weights = numpy.cumsum(sortedFitness)
+        sortedPool.sort(key=lambda x: x.fitness, reverse=True) ## sort by fitness (deacresing)
+        sortedFitness = [c.fitness for c in sortedPool] ## list of fitness values
+        ## following lines generate a weight distribution for individuals
+        weights = numpy.cumsum(sortedFitness) 
         totalFitness = sum(sortedFitness)
         percentages = [((c*100)/totalFitness) for c in weights]
+        ####
+
         total_l = len(self.population)
-        elite_l = int(total_l * (default_parameters[8] / 100)) ## use eliteSize
+        elite_l = int(total_l * (default_parameters[8] / 100)) ## use input eliteSize 
 
         for i in range(elite_l): ## add elites to new gen
             self.elite.append(sortedPool[i])
             self.selected.append(sortedPool[i])
 
-        leftToBreed = len(self.population) - len(self.elite)
+        leftToBreed = len(self.population) - len(self.elite) ## subtract the length of elites
         
         for _ in range(leftToBreed):
-            irand = random.random() * 100
+            irand = random.random() * 100 ## use weights to determine who gets selected
             for i in range(elite_l,total_l):
                 if irand <= percentages[i]:
                     self.selected.append(sortedPool[i])
                     break
         return self.selected 
 
-    def mutate(self,gen):
-        mutation_chance = default_parameters[7]
+    def mutate(self,gen): 
+        mutation_chance = default_parameters[7] ## use input mutation chance
         total_l = len(gen.population)
 
         for i in range(total_l):
@@ -163,6 +171,7 @@ class generation():
                 iMax = len(gen.population[i].route)
                 a = int(random.random() * iMax)
                 b = int(random.random() * iMax)
+                ## swap two cities in the route
                 gen.population[i].route[a], gen.population[i].route[b] = gen.population[i].route[b], gen.population[i].route[a]
         return gen
     
@@ -176,7 +185,8 @@ class generation():
         newPop.extend(self.elite)
         left = left - elite_l
 
-        for _ in range(left):
+        for _ in range(left): 
+            ## select two random individuals from selected pool to breed
             i = int(random.random() * total_l)
             j = int(random.random() * total_l)
             newInd = pool[i].breed(pool[j])
@@ -189,7 +199,7 @@ class generation():
     def __repr__(self):
         return repr(self.population) 
 
-class genBook():
+class genBook(): ## contains list of generations, cities, and some statistical data
     gen_book = []
     gen_book_mins = []
 
@@ -209,7 +219,7 @@ class genBook():
             self.gen_book_mins.append(self.gen_book[self.genCount].getMin())
             self.breedNext()
 
-    def generateCities(self):
+    def generateCities(self): ## generates random cities in the coordinate system and appends it to cities
         self.cities = []
 
         for _ in range(0,self.nCities):
@@ -219,7 +229,7 @@ class genBook():
 
         return self.cities 
 
-    def generateIndividual(self): 
+    def generateIndividual(self): ## generates random individual
         newRoute = generateRoute(self.cities)
         newIndividual = Individual(newRoute)
         newIndividual.getDistance()
@@ -235,7 +245,7 @@ class genBook():
         self.genCount = 0
         return gen_0
 
-    def breedNext(self):
+    def breedNext(self): ## breeds next generation using the current one
         self.gen_book.append(self.gen_book[self.genCount].nextGeneration(self.nInitial))
         self.genCount += 1
 
@@ -245,7 +255,7 @@ class genBook():
         plt.close()
 
     def plotRoute(self,ind,gen):
-        cities_x = [] ## print cities
+        cities_x = [] 
         cities_y = []
         city_labels = [c.name for c in self.cities]
         cities_x = [c.x for c in genBook1.cities]
